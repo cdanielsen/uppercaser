ebee23646c861fec3ea4683ae7b1b9915a77821c
Christian Danielsen <ckdanielsen@gmail.com>
2023-09-26 22:13:06 -0700

    feat: add rough impl of form for user input

--- src/client/src/App.tsx
+++ src/client/src/App.tsx
@@ -1,10 +1,56 @@
+import { ChangeEventHandler, useState } from "react";
 import "./App.css";
+import { sendMessage } from "./api";
 
 function App() {
+  const [inputText, setInputText] = useState("");
+  const [responseText, setResponseText] = useState("");
+  const [isError, setIsError] = useState(false);
+
+  const inputId = "uppercaser-request-field";
+
+  const handleInputChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+    const nextValue = e.target.value;
+    setInputText(nextValue);
+  };
+
+  const handleSubmitText = (message: string) => () => {
+    (async () => {
+      try {
+        setIsError(false);
+        const response = await sendMessage(message);
+        setResponseText(response.message);
+      } catch (e) {
+        setIsError(true);
+        setResponseText("");
+      }
+    })();
+  };
+
   return (
-    <main id="Main">
-      <h1>Hello world!</h1>
-    </main>
+    <>
+      <header>
+        <h1>UpperCaser</h1>
+      </header>
+      <main id="Main">
+        <label htmlFor={inputId}>Text to Uppercase</label>
+        <input
+          id={inputId}
+          type="text"
+          value={inputText}
+          onChange={handleInputChange}
+        />
+        <button type="button" onClick={handleSubmitText(inputText)}>
+          Submit
+        </button>
+        <section>
+          <div>
+            The server replied:{" "}
+            {isError ? "An error occurred. Please try again" : responseText}
+          </div>
+        </section>
+      </main>
+    </>
   );
 }
 
---
+++ src/client/src/api/index.ts
@@ -0,0 +1,14 @@
+interface MessageResponse {
+  message: string;
+}
+
+export const sendMessage = (message: string): Promise<MessageResponse> =>
+  fetch("/api/message", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      message,
+    }),
+  }).then((res) => res.json());
